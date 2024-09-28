8de44e0fdba40b5a6fe1c14d2c7c0198bf8b1f5f
Martin Petroski <martinpetroski@gmail.com>
2024-09-28 21:22:44 +0200

    all okay now

--- domasni/nodeExpressDomasna/app.js
+++ domasni/nodeExpressDomasna/app.js
@@ -5,35 +5,35 @@ const { f2c, c2f } = require("./functions");
 app.get("/f2c/:temperatura", (req, res) => {
   const stepeni = Number(req.params.temperatura);
   if (stepeni <= 50) {
-    return res.send(`${stepeni}°F = ${f2c(stepeni)}°C  🥶
+    return res.send(`<h1>${stepeni}°F = ${f2c(stepeni)}°C  🥶</h1> 
   `);
   } else
-    return res.send(`${stepeni}°F = ${f2c(stepeni)}°C  😎
+    return res.send(`<h1>${stepeni}°F = ${f2c(stepeni)}°C  😎</h1> 
   `);
 });
 app.get("/c2f/:temperatura", (req, res) => {
   const stepeni = Number(req.params.temperatura);
   if (stepeni <= 10) {
-    return res.send(`${stepeni}°C = ${c2f(stepeni)}°F 🥶
+    return res.send(`<h1>${stepeni}°C = ${c2f(stepeni)}°F 🥶</h1> 
   `);
   }
-  return res.send(`${stepeni}°C = ${c2f(stepeni)}°F 😎
+  return res.send(`<h1>${stepeni}°C = ${c2f(stepeni)}°F 😎</h1> 
   `);
 });
 app.get("/temp/:konverzija/:temperatura", (req, res) => {
   const stepeni = Number(req.params.temperatura);
   const konverzija = req.params.konverzija;
   if (konverzija === "c2f" && stepeni <= 10) {
-    return res.send(`${stepeni}°C = ${c2f(stepeni)}°F 🥶
+    return res.send(`<h1>${stepeni}°C = ${c2f(stepeni)}°F 🥶</h1> 
   `);
   } else if (konverzija === "c2f") {
-    return res.send(`${stepeni}°C = ${c2f(stepeni)}°F 😎
+    return res.send(`<h1>${stepeni}°C = ${c2f(stepeni)}°F 😎</h1> 
   `);
   } else if (konverzija === "f2c" && stepeni <= 50) {
-    return res.send(`${stepeni}°F = ${f2c(stepeni)}°C  🥶
+    return res.send(`<h1>${stepeni}°F = ${f2c(stepeni)}°C🥶</h1> 
   `);
   } else if (konverzija === "f2c") {
-    return res.send(`${stepeni}°F = ${f2c(stepeni)}°C  😎
+    return res.send(`<h1>${stepeni}°F = ${f2c(stepeni)}°C  😎</h1>
   `);
   } else return res.send("Zadadenata Formula ne e vo nasata databaza");
 });
