9baee5e338dbd6d11cf866ae458d621e3f246688
Martin Petroski <martinpetroski@gmail.com>
2024-09-29 18:34:03 +0200

    some another job on project express second

--- nodekurs/express_second/app.js
+++ nodekurs/express_second/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -8,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3000, (err) => {
   if (err) console.log("Invalide Page");
--- nodekurs/express_second/routes/admin.js
+++ nodekurs/express_second/routes/admin.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 router.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
-  res.send(
-    `<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>`
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 router.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+  res.sendFile(path.join(__dirname, "../", "views", "404.html"));
 });
 module.exports = router;
