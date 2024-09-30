7c03b32dd25ab8c2e42c59892c1d0c45b793d9cd
Martin Petroski <martinpetroski@gmail.com>
2024-09-30 22:40:44 +0200

    completed domasnaExpressRemake

--- domasni/domasnaExpressRemake/app.js
+++ domasni/domasnaExpressRemake/app.js
@@ -21,13 +21,23 @@ app.get("/c2f/:temperatura", (req, res) => {
 app.get("/temp/:konverzija/:temperatura", (req, res) => {
   const stepeni = Number(req.params.temperatura);
   const konverzija = req.params.konverzija;
+  if (konverzija === "c2f") {
+    return res.send(
+      stepeni <= 10
+        ? `<h1>${stepeni}°C = ${c2f(stepeni)}°F 🥶</h1> `
+        : `<h1>${stepeni}°C = ${c2f(stepeni)}°F 😎</h1> `
+    );
+  } else if (konverzija === "f2c") {
+    return res.send(
+      stepeni <= 50
+        ? `<h1>${stepeni}°F = ${f2c(stepeni)}°C 🥶</h1> `
+        : `<h1>${stepeni}°F = ${f2c(stepeni)}°C 😎</h1> `
+    );
+  }
+});
+app.use((req, res) => {
   res.send(
-    switch konverzija{
-      
-    }
-    stepeni <= 10
-      ? `<h1>${stepeni}°C = ${c2f(stepeni)}°F 🥶</h1> `
-      : `<h1>${stepeni}°C = ${c2f(stepeni)}°F 😎</h1> `
+    `<img src="https://media.istockphoto.com/id/1411798446/vector/modern-coming-soon-under-construction-sticker-banner.jpg?s=612x612&w=0&k=20&c=KgQ9eYEfuzTdmc5ypmMYoz8JHPTbe_TjEXxyxNixKQ4=" alt="">`
   );
 });
 
