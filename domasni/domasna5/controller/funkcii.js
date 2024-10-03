058f2f72c4a57546c466a0be691d1baf3326dc5f
Martin Petroski <martinpetroski@gmail.com>
2024-10-03 20:10:34 +0200

    domasna5

---
+++ domasni/domasna5/controller/funkcii.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+// const dataObj = require("../app");
+// console.log(dataObj);
+
+
+// const bmiCalculator = async (req, res) => {
+//   let prosek = 0;
+//   const height = req.params.height;
+//   const bmi = weight / (height * height);
+//   const response = await parseTemplate("calculator.html", {
+//     data: bmi,
+//     ime: "BMI CALCULATOR",
+//   });
+//   res.send(response);
+// };
+// // const calculateForce = async();
+
+// module.exports = {
+//   bmiCalculator,
+// };
