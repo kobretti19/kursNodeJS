79ccaf6eee4e6278ded956ca65cd7ddc5950f3a5
Martin Petroski <martinpetroski@gmail.com>
2024-10-04 00:05:16 +0200

    domasna 9

---
+++ predavanja/c9/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const {
+  getTest,
+  postTest1,
+  postTest2,
+  postTest3,
+  postTest4,
+} = require("./controller/rutiranje");
+// const postTest1 = require("./controller/rutiranje");
+app.get("/vezba", getTest);
+app.post("/vezba", getTest);
+app.post("/vezba1", postTest1);
+app.post("/vezba2", postTest2);
+app.post("/vezba3", postTest3);
+app.post("/vezba4", postTest4);
+
+app.listen(3000, (err) => {
+  if (err) {
+    return console.log("Server Internal error");
+  }
+  return console.log("Server started on port:3000");
+});
+//ace.ki@hotmail.com
